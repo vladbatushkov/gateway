20c4ea461a6d452475d5cecffcb2e4b55de4b228
Vlad Batushkov <vlad.batushkov@agoda.com>
2022-11-22 23:15:05 +0700

    fix ws

--- src/web-client/src/components/Tags.tsx
+++ src/web-client/src/components/Tags.tsx
@@ -1,8 +1,28 @@
-import React from 'react';
-import { useGetTagsQuery } from '../graphql/generated/schema';
+import React, { Key, useEffect, useState } from 'react';
+import { useGetTagsQuery, useTagAddedSubscription } from '../graphql/generated/schema';
+
+type Tag = {
+  id: Key,
+  name: String
+}
 
 export const Tags: React.FC = () => {
-  const { loading, error, data } = useGetTagsQuery();
+
+  const sub = useTagAddedSubscription();
+  const dataSub = sub.data;
+
+  const { data, loading, error } = useGetTagsQuery();
+  const [tags, setTags] = useState([] as Tag[]);
+
+  useEffect(() => {
+    if (!tags || !dataSub?.tagAdded) return;
+    setTags([...tags, dataSub?.tagAdded as Tag]);
+  }, [dataSub]);
+
+  useEffect(() => {
+    if (!data?.tags) return;
+    setTags(data?.tags as Tag[]);
+  }, [data]);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error : {error.message}</p>;
@@ -10,8 +30,8 @@ export const Tags: React.FC = () => {
   return (<div>
     <h2>Tags:</h2>
     <ul>
-      {data?.tags.map(({ id, name }) => (
-        <li key={id}>{name}</li>
+      {tags?.map((item: Tag) => (
+        item ? <li key={item.id}>{item.name}</li> : null
       ))}
     </ul>
   </div>);
